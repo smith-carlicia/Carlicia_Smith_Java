d705c5033f3de52dbf013ebb5d3678a16359e676
Carlicia Smith <smith.carlicia@gmail.com>
2021-06-19 14:03:50 -0400

    updated ConverterSwitch and ConverterApplication file

--- M1-Challenge-Smith-Carlicia/ConverterInterface/src/main/java/com/company/ConverterApplication.java
+++ M1-Challenge-Smith-Carlicia/ConverterInterface/src/main/java/com/company/ConverterApplication.java
@@ -4,7 +4,14 @@ public class ConverterApplication {
 
     public static void main(String[] args) {
 
-        int month;
-        int day;
+        ConverterIf month1 = new ConverterIf();
+        ConverterIf day1 = new ConverterIf();
+        System.out.println(month1.convertMonth(12));
+        System.out.println(day1.convertDay(7));
+
+        ConverterSwitch month2 = new ConverterIf();
+        ConvertSwitch day2 = new ConverterIf();
+        System.out.println(month2.convertMonth(12));
+        System.out.println(day2.convertDay(6));
     }
 }
