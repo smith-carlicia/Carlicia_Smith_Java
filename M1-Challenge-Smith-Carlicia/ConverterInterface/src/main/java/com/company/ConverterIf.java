f2db309072bf876dfbad1900d41eba1de99bd94a
Carlicia Smith <smith.carlicia@gmail.com>
2021-06-19 13:45:23 -0400

    updated ConverterIf file

--- M1-Challenge-Smith-Carlicia/ConverterInterface/src/main/java/com/company/ConverterIf.java
+++ M1-Challenge-Smith-Carlicia/ConverterInterface/src/main/java/com/company/ConverterIf.java
@@ -1,8 +1,67 @@
 package com.company;
 
-public class ConverterIf<convertMonth> {
-    
-    if(month < convertMonth) {
-        System.out.println(monthNumber);
-    };
+public class ConverterIf implements Converter {
+
+    public String convertMonth(int monthNum) {
+
+        int i = monthNum;
+
+        String monthString = "";
+
+        if(i == 1) {
+            monthString = "January";
+        } else if (i == 2) {
+            monthString = "February";
+        } else if (i == 3) {
+            monthString = "March";
+        } else if (i == 4) {
+            monthString = "April";
+        } else if (i == 5) {
+            monthString = "May";
+        } else if (i == 6) {
+            monthString = "June";
+        } else if (i == 7) {
+            monthString = "July";
+        } else if (i == 8) {
+            monthString = "August";
+        } else if (i == 9) {
+            monthString = "September";
+        } else if (i == 10) {
+            monthString = "October";
+        } else if (i == 11) {
+            monthString = "November";
+        } else if (i = 12) {
+            monthString = "December";
+        } else {
+            monthString = "Invalid month entry"
+        }
+            return "ConverterIf:" + monthString;
+    }
+
+
+    public String convertDay(int dayNum) {
+        int i = dayNum;
+
+        String dayString = "";
+
+        if(i == 1) {
+            dayString = "Sunday"
+        } else if (i == 2) {
+            dayString = "Monday"
+        } else if (i == 3) {
+            dayString = "Tuesday"
+        } else if (i == 4) {
+            dayString = "Wednesday"
+        } else if (i == 5) {
+            dayString = "Thursday"
+        } else if (i == 6) {
+            dayString = "Friday"
+        } else if (i == 7) {
+            dayString = "Saturday"
+        } else {
+            dayString = "Invalid day entry"
+        }
+
+        return "ConverterIf:" + dayString;
+    }
 }
